ff9ee5876c5d1b094525f267b7b64e8c9f4e025a
Jimjam89 <Jamesthornlea92@gmail.com>
2022-06-09 05:26:38 +0100

    Export components (#26)
    Export Notification and NotificationGroup to increase package flexibility

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { install } from './plugin'
 export { notify } from './notify'
+export { default as NotificationGroup } from './NotificationGroup.vue'
+export { default as Notification } from './Notification.vue'
 
 export default {
   install,

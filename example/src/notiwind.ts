4dd72115361bbc607ee66e6eea3438529f7dfd15
nd0ut <nd0ut.me@gmail.com>
2023-01-18 19:45:17 +0400

    chore: update example project

---
+++ example/src/notiwind.ts
@@ -0,0 +1,11 @@
+import {
+  createNotifier,
+  NotificationGroup,
+  defineNotificationComponent,
+  // eslint-disable-next-line import/no-relative-packages
+} from "../..";
+import { NotificationSchema } from "./types";
+
+export const notify = createNotifier<NotificationSchema>();
+export const Notification = defineNotificationComponent<NotificationSchema>();
+export { NotificationGroup };
